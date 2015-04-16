3067a917f1f852e6fa069ee9cd33f6b7d3e161c9
trykowski1 <jaroslaw.trykowski@mobica.com>
2015-04-16 10:07:05 +0200

    Revert "Pull1"

---
+++ src/com/mobica/jatr/ForthProj/Sun.java
@@ -0,0 +1,11 @@
+package com.mobica.jatr.ForthProj;
+
+public class Sun extends Star {
+
+	@Override
+	public Float getShinningRange() {
+		Float i = new Float(1);
+		return i;
+	}
+
+}
---
+++ src/com/mobica/jatr/ForthProj/UltraSun.java
@@ -0,0 +1,9 @@
+package com.mobica.jatr.ForthProj;
+
+public class UltraSun extends Sun {
+	
+	public String getUltraSun(){
+		String s = "UltraSunHere";
+		return s;
+	}
+}
